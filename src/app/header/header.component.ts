7936d4b8fecb60dea7e7b8ac221fca7f886d52aa
Donald W <github@donaldw.com>
2025-11-30 22:46:32 +0000

    Improve header navigation controls

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { IsActiveMatchOptions, Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
-    standalone: true,
-    imports: [RouterLink]
+    standalone: true
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+    private readonly exactMatchOptions: IsActiveMatchOptions = {
+        paths: 'exact',
+        queryParams: 'ignored',
+        fragment: 'ignored',
+        matrixParams: 'ignored'
+    };
+
+    constructor(private readonly router: Router) {}
+
+    goToMenuOrGame(): void {
+        this.router.navigate([this.isInMenu ? '/game' : '/menu']);
+    }
+
+    goToHelpOrGame(): void {
+        this.router.navigate([this.isInHelp ? '/game' : '/help']);
+    }
+
+    goToGame(): void {
+        this.router.navigate(['/game']);
+    }
+
+    private get isInMenu(): boolean {
+        return this.router.isActive('/menu', this.exactMatchOptions);
+    }
+
+    private get isInHelp(): boolean {
+        return this.router.isActive('/help', this.exactMatchOptions);
+    }
+}
