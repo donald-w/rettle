9bca7981ce715fc0463cd48d10357a66fc577c53
Donald Walker <github@donaldw.com>
2025-12-01 22:49:33 +0000

    Remove unused deprecated provideAnimations module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode, provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { routes } from './app/app-routing.module';
@@ -14,7 +13,6 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
-    provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes, withHashLocation()),
   ],
